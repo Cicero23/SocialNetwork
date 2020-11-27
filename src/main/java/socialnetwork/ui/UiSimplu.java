d1bdaabd2cb0a7b6a13a981f0a476800b5a30884
Dinu <viorel.dinu0@yahoo.com>
2020-11-27 12:26:11 +0200

    before lab 6-7

--- src/main/java/socialnetwork/ui/UiSimplu.java
+++ src/main/java/socialnetwork/ui/UiSimplu.java
@@ -29,8 +29,6 @@ public class UiSimplu {
         while (true){
             System.out.print(">>");
             String s = in.nextLine();
-            int ceva = 5;
-            //ceva mesaj
             if(s.equals("exit")){
                 in.close();
                 break;
